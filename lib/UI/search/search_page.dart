7488b56794d8d70b1391e823cf37e4eb5c3aa1e1
hossein.naserizade <hnaseri.iust@gmail.com>
2022-06-18 00:25:55 +0430

    add search apis

---
+++ lib/UI/search/search_page.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:honar_gallary/logic/general_values.dart';
+import 'package:honar_gallary/state_managment/search/search_cubit.dart';
+import 'package:nb_utils/nb_utils.dart';
+
+import 'components/search_text_field.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<SearchCubit>(
+      create: (context) => SearchCubit(),
+      child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          body: Directionality(
+            textDirection: TextDirection.rtl,
+            child: Scaffold(
+              resizeToAvoidBottomInset: false,
+              body: GestureDetector(
+                onTap: () {
+                  FocusScope.of(context).unfocus();
+                },
+                child: Container(
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color.fromRGBO(4, 9, 35, 1),
+                          Color.fromRGBO(39, 105, 171, 1),
+                        ],
+                        begin: FractionalOffset.bottomCenter,
+                        end: FractionalOffset.topCenter,
+                      ),
+                    ),
+                    child: Stack(
+                      children: [
+                        SizedBox(
+                          height: 100,
+                          child: Align(
+                            alignment: const Alignment(0, 1.5),
+                            child: TextFieldSearchWidget(
+                              onChanged: (value) {},
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: context.height(),
+                          width: context.width(),
+                          child: DraggableScrollableSheet(
+                              expand: true,
+                              minChildSize: 0.8,
+                              initialChildSize: 0.82,
+                              builder: (context, listScrollController) {
+                                return Container(
+                                    decoration: boxDecorationWithRoundedCorners(
+                                        backgroundColor: Colors.white,
+                                        borderRadius: const BorderRadius.only(
+                                            topRight: Radius.circular(40),
+                                            topLeft: Radius.circular(40))),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: BodySearchWidget(
+                                          controller: listScrollController),
+                                    ));
+                              }),
+                        )
+                      ],
+                    )),
+              ),
+            ),
+          )),
+    );
+  }
+}
+
+class BodySearchWidget extends StatefulWidget {
+  final ScrollController controller;
+
+  const BodySearchWidget({Key? key, required this.controller})
+      : super(key: key);
+
+  @override
+  State<BodySearchWidget> createState() => _BodySearchWidgetState();
+}
+
+class _BodySearchWidgetState extends State<BodySearchWidget> {
+  int categoryItemSelectedId = -1;
+  late bool startApp;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startApp = true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var categories = ConfigGeneralValues.configGeneralValues?.getCategories();
+    return BlocBuilder<SearchCubit, SearchState>(
+      builder: (context, state) {
+        if (state is SearchInitial && startApp) {
+          BlocProvider.of<SearchCubit>(context).search("sl");
+          startApp = false;
+          // return Container();
+        }
+        if (state is SearchLoaded) {
+          return ListView.builder(
+            controller: widget.controller,
+            scrollDirection: Axis.vertical,
+            padding: const EdgeInsets.only(bottom: 70, top: 50),
+            physics: const ClampingScrollPhysics(),
+            itemBuilder: (context, index) {
+              return Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                child: Column(
+                  children: [
+                    Text(
+                      state.artPieces[index].title,
+                      style: TextStyle(
+                          color: Colors.pink, fontWeight: FontWeight.w900),
+                    ),
+                    Text(
+                      state.artPieces[index].title,
+                      style: TextStyle(
+                          color: Colors.pink, fontWeight: FontWeight.w900),
+                    ),
+                    Text(
+                      state.artPieces[index].owner.fullName,
+                      style: TextStyle(
+                          color: Colors.pink, fontWeight: FontWeight.w900),
+                    ),
+                  ],
+                ),
+              );
+            },
+            itemCount: state.artPieces.length,
+          );
+        }
+
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            CircularProgressIndicator(
+              color: Colors.pink,
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  bool checIndex(int index) =>
+      categoryItemSelectedId == index ||
+      (categoryItemSelectedId == -1 && index == 0);
+}
---
+++ lib/state_managment/search/search_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:honar_api_v13/api.dart';
+import 'package:honar_gallary/logic/consts.dart';
+import 'package:meta/meta.dart';
+
+part 'search_state.dart';
+
+class SearchCubit extends Cubit<SearchState> {
+  late ArtApi artApi;
+
+  SearchCubit() : super(SearchInitial()) {
+    artApi = ArtApi(interfaceOfUser);
+  }
+
+  Future<void> search(String text) async {
+    try {
+      emit(SearchLoading());
+      SearchResult searchResult = await artApi.artSearchList(query: text);
+      print(searchResult);
+      emit(SearchLoaded(searchResult.artists, searchResult.artPieces));
+    } catch (e) {
+      emit(SearchError());
+    }
+  }
+}
