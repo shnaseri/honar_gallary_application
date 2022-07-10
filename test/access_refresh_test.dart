2614e0282f8d44f6495205916f752957453c8593
hossein.naserizade <hnaseri.iust@gmail.com>
2022-07-10 21:32:46 +0430

    add test files and fix otp

---
+++ test/access_refresh_test.dart
@@ -0,0 +1,32 @@
+//
+// AUTO-GENERATED FILE, DO NOT MODIFY!
+//
+// @dart=2.12
+
+// ignore_for_file: unused_element, unused_import
+// ignore_for_file: always_put_required_named_parameters_first
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: lines_longer_than_80_chars
+
+import 'package:honar_api_v17/api.dart';
+import 'package:test/test.dart';
+
+// tests for AccessRefresh
+void main() {
+  // final instance = AccessRefresh();
+
+  group('test AccessRefresh', () {
+    // String access
+    test('to test the property `access`', () async {
+      // TODO
+    });
+
+    // String refresh
+    test('to test the property `refresh`', () async {
+      // TODO
+    });
+
+
+  });
+
+}
---
+++ test/profile_api_test.dart
@@ -0,0 +1,46 @@
+//
+// AUTO-GENERATED FILE, DO NOT MODIFY!
+//
+// @dart=2.12
+
+// ignore_for_file: unused_element, unused_import
+// ignore_for_file: always_put_required_named_parameters_first
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: lines_longer_than_80_chars
+
+import 'package:honar_api_v17/api.dart';
+import 'package:test/test.dart';
+
+
+/// tests for ProfileApi
+void main() {
+  // final instance = ProfileApi();
+
+  group('tests for ProfileApi', () {
+    //Future<ArtArtPieceUpdate200Response> profileAddProfileImageCreate(AddImage data) async
+    test('test profileAddProfileImageCreate', () async {
+      // TODO
+    });
+
+    //Future<ProfileFollowUpdate200Response> profileFollowUpdate(String id) async
+    test('test profileFollowUpdate', () async {
+      // TODO
+    });
+
+    //Future<FullUser> profilePartialUpdate(int id, FullUser data) async
+    test('test profilePartialUpdate', () async {
+      // TODO
+    });
+
+    //Future<FullUser> profileRead(int id) async
+    test('test profileRead', () async {
+      // TODO
+    });
+
+    //Future<FullUser> profileUpdate(int id, FullUser data) async
+    test('test profileUpdate', () async {
+      // TODO
+    });
+
+  });
+}
